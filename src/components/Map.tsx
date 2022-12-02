20ff9f19d035e9bb497a2e4937f6d244ceec5323
Nicolas Ettlin <nicolas.ettlin@me.com>
2022-12-02 02:15:10 -0500

    Add storybooks

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
-export default function Map() {
+interface MapProps {
+  /**
+   * The token provided by MapKit JS
+   */
+  token: string;
+}
+
+export default function Map({
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  token,
+}: MapProps) {
   return (
     <React.StrictMode>
       <div />

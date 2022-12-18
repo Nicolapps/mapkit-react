ca5997cb47a51b7beb3366cfa0668050afe6ed22
Nicolas Ettlin <nicolas.ettlin@me.com>
2022-12-18 00:00:28 -0500

    Fix Marker exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { default as Map } from './components/Map';
 export { default as MapProps } from './components/MapProps';
-export { default as Annotation } from './components/Marker';
-export { default as AnnotationProps } from './components/MarkerProps';
+export { default as Marker } from './components/Marker';
+export { default as MarkerProps } from './components/MarkerProps';
 export {
   ColorScheme, MapType, Distances, LoadPriority,
 } from './util/parameters';

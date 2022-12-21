185f65714c895809b3248389ec1b525772c66bf5
Nicolas Ettlin <nicolas.ettlin@me.com>
2022-12-21 19:27:42 +0100

    Add new stories for Map

--- src/stories/Map.stories.tsx
+++ src/stories/Map.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ComponentMeta, Story } from '@storybook/react';
 
 import Map from '../components/Map';
+import { ColorScheme, MapType } from '../util/parameters';
 
 const token = process.env.STORYBOOK_MAPKIT_JS_TOKEN!;
 export default {
@@ -18,3 +19,27 @@ type MapProps = React.ComponentProps<typeof Map>;
 const Template: Story<MapProps> = (args) => <Map {...args} />;
 
 export const Empty = Template.bind({});
+
+export const CustomizedAppearance = Template.bind({});
+CustomizedAppearance.args = {
+  colorScheme: ColorScheme.Dark,
+  mapType: MapType.MutedStandard,
+  showsMapTypeControl: false,
+  showsUserLocationControl: true,
+  paddingLeft: 16,
+  paddingRight: 16,
+  paddingTop: 32,
+  paddingBottom: 32,
+};
+
+export const RegionLock = Template.bind({});
+RegionLock.args = {
+  cameraBoundary: {
+    centerLatitude: 40.444,
+    centerLongitude: -79.945,
+    latitudeDelta: 0.006,
+    longitudeDelta: 0.008,
+  },
+  minCameraDistance: 100,
+  maxCameraDistance: 1000,
+};

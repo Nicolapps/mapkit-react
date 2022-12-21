db4da411f2d011363e9fc98ff682b9d1fec58000
Nicolas Ettlin <nicolas.ettlin@me.com>
2022-12-21 17:41:31 -0500

    Improve Map + Marker storybooks (#11)
    * Show the stories source code
    
    * Improve stories source code style
    
    * Fix parameter binding in the marker story
    
    Closes #10
    
    * Improve stories typing
    
    * Add new stories for Map
    
    * Fix enum parameters in Storybook
    
    Fixes #7

--- src/stories/Map.stories.tsx
+++ src/stories/Map.stories.tsx
@@ -1,22 +1,59 @@
 import React from 'react';
-import { ComponentMeta } from '@storybook/react';
+import { ComponentMeta, Story } from '@storybook/react';
 
 import Map from '../components/Map';
+import {
+  ColorScheme, MapType, Distances, LoadPriority,
+} from '../util/parameters';
 
+const token = process.env.STORYBOOK_MAPKIT_JS_TOKEN!;
+
+const enumArgType = (e: object) => ({
+  options: Object.values(e).filter((val) => typeof val === 'number'),
+  control: {
+    type: 'inline-radio',
+    labels: Object.values(e).filter((val) => typeof val === 'string'),
+  },
+});
 export default {
+  title: 'Components/Map',
   component: Map,
-  args: {
-    token: process.env.STORYBOOK_MAPKIT_JS_TOKEN,
+  args: { token },
+  argTypes: {
+    colorScheme: enumArgType(ColorScheme),
+    mapType: enumArgType(MapType),
+    distances: enumArgType(Distances),
+    loadPriority: enumArgType(LoadPriority),
   },
-  parameters: {
-    layout: 'fullscreen',
-  },
+  parameters: { layout: 'fullscreen' },
 } as ComponentMeta<typeof Map>;
 
 type MapProps = React.ComponentProps<typeof Map>;
 
-function Template(args: MapProps) {
-  return <Map {...args} />;
-}
+const Template: Story<MapProps> = (args) => <Map {...args} />;
 
 export const Empty = Template.bind({});
+
+export const CustomizedAppearance = Template.bind({});
+CustomizedAppearance.args = {
+  colorScheme: ColorScheme.Dark,
+  mapType: MapType.MutedStandard,
+  showsMapTypeControl: false,
+  showsUserLocationControl: true,
+  paddingLeft: 16,
+  paddingRight: 16,
+  paddingTop: 32,
+  paddingBottom: 32,
+};
+
+export const RegionLock = Template.bind({});
+RegionLock.args = {
+  cameraBoundary: {
+    centerLatitude: 40.444,
+    centerLongitude: -79.945,
+    latitudeDelta: 0.006,
+    longitudeDelta: 0.008,
+  },
+  minCameraDistance: 100,
+  maxCameraDistance: 1000,
+};
--- src/stories/Marker.stories.tsx
+++ src/stories/Marker.stories.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { ComponentMeta } from '@storybook/react';
+import { ComponentMeta, Story } from '@storybook/react';
 
 import Map from '../components/Map';
 import Marker from '../components/Marker';
+import { FeatureVisibility } from '../util/parameters';
 
+const enumArgType = (e: object) => ({
+  options: Object.values(e).filter((x) => typeof x === 'string'),
+  mapping: e,
+});
 export default {
+  title: 'Components/Marker',
   component: Marker,
   args: {},
+  argTypes: {
+    subtitleVisibility: enumArgType(FeatureVisibility),
+    titleVisibility: enumArgType(FeatureVisibility),
+  },
   parameters: {
     layout: 'fullscreen',
   },
@@ -14,15 +24,11 @@ export default {
 
 type MarkerProps = React.ComponentProps<typeof Marker>;
 
-function Template(args: MarkerProps) {
-  return (
-    <Map token={process.env.STORYBOOK_MAPKIT_JS_TOKEN!}>
-      <Marker {...args} />
-    </Map>
-  );
-}
+const Template: Story<MarkerProps> = (args) => (
+  <Map token={process.env.STORYBOOK_MAPKIT_JS_TOKEN!}>
+    <Marker {...args} />
+  </Map>
+);
 
-export const Default = Template.bind({
-  latitude: 46.52,
-  longitude: 6.57,
-});
+export const Default = Template.bind({});
+Default.args = { latitude: 46.52, longitude: 6.57 };

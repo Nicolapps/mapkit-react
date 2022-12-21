556ba063907de8c98e2546d07d6f1fafe679739a
Nicolas Ettlin <nicolas.ettlin@me.com>
2022-12-21 23:02:49 +0100

    Fix enum parameters in Storybook
    Fixes #7

--- src/stories/Map.stories.tsx
+++ src/stories/Map.stories.tsx
@@ -2,16 +2,30 @@ import React from 'react';
 import { ComponentMeta, Story } from '@storybook/react';
 
 import Map from '../components/Map';
-import { ColorScheme, MapType } from '../util/parameters';
+import {
+  ColorScheme, MapType, Distances, LoadPriority,
+} from '../util/parameters';
 
 const token = process.env.STORYBOOK_MAPKIT_JS_TOKEN!;
+
+const enumArgType = (e: object) => ({
+  options: Object.values(e).filter((val) => typeof val === 'number'),
+  control: {
+    type: 'inline-radio',
+    labels: Object.values(e).filter((val) => typeof val === 'string'),
+  },
+});
 export default {
   title: 'Components/Map',
   component: Map,
   args: { token },
-  parameters: {
-    layout: 'fullscreen',
+  argTypes: {
+    colorScheme: enumArgType(ColorScheme),
+    mapType: enumArgType(MapType),
+    distances: enumArgType(Distances),
+    loadPriority: enumArgType(LoadPriority),
   },
+  parameters: { layout: 'fullscreen' },
 } as ComponentMeta<typeof Map>;
 
 type MapProps = React.ComponentProps<typeof Map>;
--- src/stories/Marker.stories.tsx
+++ src/stories/Marker.stories.tsx
@@ -3,11 +3,20 @@ import { ComponentMeta, Story } from '@storybook/react';
 
 import Map from '../components/Map';
 import Marker from '../components/Marker';
+import { FeatureVisibility } from '../util/parameters';
 
+const enumArgType = (e: object) => ({
+  options: Object.values(e).filter((x) => typeof x === 'string'),
+  mapping: e,
+});
 export default {
   title: 'Components/Marker',
   component: Marker,
   args: {},
+  argTypes: {
+    subtitleVisibility: enumArgType(FeatureVisibility),
+    titleVisibility: enumArgType(FeatureVisibility),
+  },
   parameters: {
     layout: 'fullscreen',
   },

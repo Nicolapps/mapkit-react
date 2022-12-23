312f354c6b454407d7c13877422bd7ab216ee1d3
Nicolas Ettlin <nicolas.ettlin@me.com>
2022-12-24 00:24:58 +0100

    Add an example using forward refs

--- src/stories/Map.stories.tsx
+++ src/stories/Map.stories.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { ComponentMeta, Story } from '@storybook/react';
+import './stories.css';
 
 import Map from '../components/Map';
 import {
@@ -56,4 +57,45 @@ RegionLock.args = {
   },
   minCameraDistance: 100,
   maxCameraDistance: 1000,
+};
+
+export const MapObject: Story<MapProps> = (args) => {
+  const mapRef = useRef<mapkit.Map>(null);
+
+  // Set the initial region to San Francisco
+  const ready = useCallback(() => {
+    mapRef.current!.region = new mapkit.CoordinateRegion(
+      new mapkit.Coordinate(37.7812, -122.44755),
+      new mapkit.CoordinateSpan(0.10, 0.11),
+    );
+  }, [mapRef]);
+
+  // Animate a rotation when the button is pressed
+  const rotate = useCallback(() => {
+    if (mapRef.current === null) return;
+
+    if (!mapRef.current.isRotationAvailable) {
+      // eslint-disable-next-line no-alert
+      alert('Sorry, rotation is not available.');
+      return;
+    }
+
+    mapRef.current.setRotationAnimated(mapRef.current.rotation + 90);
+  }, [mapRef]);
+
+  return (
+    <>
+      <Map ref={mapRef} {...args} onReady={ready} />
+
+      <div className="map-overlay">
+        <button type="button" onClick={rotate}>
+          ↻ Rotate
+        </button>
+      </div>
+    </>
+  );
+};
+MapObject.storyName = 'Use the Map Object';
+MapObject.args = {
+  paddingBottom: 44,
 };

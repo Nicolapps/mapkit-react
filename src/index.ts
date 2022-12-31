58f89b78537dc34076f7a66f9c41b100fa791881
Nicolas Ettlin <nicolas.ettlin@me.com>
2022-12-31 12:02:22 +0100

    Move CoordinateRegion to parameters.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export { default as MapProps } from './components/MapProps';
 export { default as Marker } from './components/Marker';
 export { default as MarkerProps } from './components/MarkerProps';
 export {
-  ColorScheme, MapType, Distances, LoadPriority,
+  ColorScheme, MapType, Distances, LoadPriority, CoordinateRegion,
 } from './util/parameters';

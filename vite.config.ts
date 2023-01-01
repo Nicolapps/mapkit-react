a8364c48bd82c7d47fe4f7ee8f5a3f37e4c3bca7
Nicolas Ettlin <nicolas.ettlin@me.com>
2023-01-01 01:22:31 +0100

    Generate TypeScript type files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    dts(),
+    react(),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),

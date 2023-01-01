3038bd6b083dcd9674d97c77cb7dc9f72e0e804f
Nicolas Ettlin <nicolas.ettlin@me.com>
2023-01-01 22:11:00 +0100

    Export FeatureVisibility

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export type { default as MapProps } from './components/MapProps';
 export { default as Marker } from './components/Marker';
 export type { default as MarkerProps } from './components/MarkerProps';
 export {
-  ColorScheme, MapType, Distances, LoadPriority,
+  ColorScheme, MapType, Distances, LoadPriority, FeatureVisibility,
 } from './util/parameters';
 export type { CoordinateRegion } from './util/parameters';

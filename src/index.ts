78e836bdaf17ba39095613c5ca0852ba377d2fab
Derek Reynolds <derekr@users.noreply.github.com>
2023-02-02 11:50:33 -0800

    Export Annotation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export { default as Map } from './components/Map';
 export type { default as MapProps } from './components/MapProps';
 export { default as Marker } from './components/Marker';
 export type { default as MarkerProps } from './components/MarkerProps';
+export { default as Annotation } from './components/Annotation';
+export type { default as AnnotationProps } from './components/AnnotationProps';
 export {
   ColorScheme, MapType, Distances, LoadPriority, FeatureVisibility,
 } from './util/parameters';

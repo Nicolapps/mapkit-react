5f9e005495ebe8e1f5036ba8955b3dc614e32162
Derek Reynolds <derekr@users.noreply.github.com>
2023-02-02 20:26:07 -0800

    SVG feedback

--- src/stories/Annotation.stories.tsx
+++ src/stories/Annotation.stories.tsx
@@ -7,16 +7,17 @@ import { CoordinateRegion, FeatureVisibility } from '../util/parameters';
 
 const token = process.env.STORYBOOK_MAPKIT_JS_TOKEN!;
 
+// SVG from https://webkul.github.io/vivid
 function CustomMarker() {
   return (
     <svg width="24px" height="24px" viewBox="-4 0 36 36" version="1.1" xmlns="http://www.w3.org/2000/svg">
-      <g id="Vivid.JS" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-        <g id="Vivid-Icons" transform="translate(-125.000000, -643.000000)">
-          <g id="Icons" transform="translate(37.000000, 169.000000)">
-            <g id="map-marker" transform="translate(78.000000, 468.000000)">
+      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <g transform="translate(-125.000000, -643.000000)">
+          <g transform="translate(37.000000, 169.000000)">
+            <g transform="translate(78.000000, 468.000000)">
               <g transform="translate(10.000000, 6.000000)">
                 <path d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z" id="Shape" fill="#FF6E6E" />
-                <circle id="Oval" fill="#0C0058" fillRule="nonzero" cx="14" cy="14" r="7" />
+                <circle fill="#0C0058" fillRule="nonzero" cx="14" cy="14" r="7" />
               </g>
             </g>
           </g>

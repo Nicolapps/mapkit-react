26132ef8ef38fef1c4c0a699250a1206af37ca49
Nicolas Ettlin <nicolas.ettlin@me.com>
2023-04-17 19:40:44 -0400

    Remove trailing space

--- src/components/MarkerProps.tsx
+++ src/components/MarkerProps.tsx
@@ -87,7 +87,7 @@ export default interface MarkerProps {
   onDeselect?: () => void;
 
   /**
-  * A shared identifier for all of the member annotations. 
+  * A shared identifier for all of the member annotations.
   * An annotation needs a clusteringIdentifier to be part of an annotation cluster.
   * @see {@link https://developer.apple.com/documentation/mapkitjs/mapkit/annotations/clustering_annotations}
   */

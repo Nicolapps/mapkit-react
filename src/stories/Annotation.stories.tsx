547addda74b1b0f19e9be28d0436bb8d0545cc87
Nikischin <tim.nikischin@web.de>
2024-04-07 10:49:43 +0700

    Implement Annotation Storybook example

--- src/stories/Annotation.stories.tsx
+++ src/stories/Annotation.stories.tsx
@@ -129,3 +129,44 @@ export const AnimatedAnnotation = () => {
 };
 
 AnimatedAnnotation.storyName = 'Animated Annotation';
+
+function CustomCalloutElement(
+  { title, subtitle, url }: { title: string, subtitle: string, url: string },
+) {
+  return (
+    <div className="landmark">
+      <h1>{title ?? ''}</h1>
+      <section>
+        <p>{subtitle ?? ''}</p>
+        <p><a href={url} target="_blank" rel="noreferrer">Website</a></p>
+      </section>
+    </div>
+  );
+}
+
+export const CustomAnnotationCallout = () => {
+  const initialRegion: CoordinateRegion = useMemo(() => ({
+    centerLatitude: 46.20738751546706,
+    centerLongitude: 6.155891756231,
+    latitudeDelta: 0.007,
+    longitudeDelta: 0.015,
+  }), []);
+
+  return (
+    <Map token={token} initialRegion={initialRegion}>
+      <Annotation
+        latitude={46.20738751546706}
+        longitude={6.155891756231}
+        title="Jet d’eau"
+        subtitle="Iconic landmark of Geneva"
+        calloutElement={<CustomCalloutElement title="Jet d’eau" subtitle="Iconic landmark of Geneva" url="https://en.wikipedia.org/wiki/Jet_d%27Eau" />}
+        calloutEnabled
+        calloutOffsetX={-148}
+        calloutOffsetY={-82}
+      >
+        <CustomMarker />
+      </Annotation>
+    </Map>
+  );
+};
+CustomAnnotationCallout.storyName = 'Annotation with custom callout element';
